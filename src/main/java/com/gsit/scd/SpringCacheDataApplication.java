d414842a2e752ee743389d9b990211eee5be7ca6
shanmukhanand <s.barsagadey@gmail.com>
2018-03-19 20:55:45 +0530

    Update SpringCacheDataApplication.java

--- src/main/java/com/gsit/scd/SpringCacheDataApplication.java
+++ src/main/java/com/gsit/scd/SpringCacheDataApplication.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
@@ -13,20 +15,26 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableCaching
-public class SpringCacheDataApplication {
+public class SpringCacheDataApplication  extends SpringBootServletInitializer {
 	
 	@Bean
 	public CacheManager cacheManager() {
-
 		Cache cache = new ConcurrentMapCache("findByIsbn");
-
 		SimpleCacheManager manager = new SimpleCacheManager();
 		manager.setCaches(Arrays.asList(cache));
-
 		return manager;
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringCacheDataApplication.class);
+    }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringCacheDataApplication.class, args);
+    }
+
+	/*public static void main(String[] args) {
 		SpringApplication.run(SpringCacheDataApplication.class, args);
-	}
+	}*/
 }
